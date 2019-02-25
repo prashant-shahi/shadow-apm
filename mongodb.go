549392b176467197997b58004ab9d8f7eb7cb1f7
Prashant Shahi <PrashantShahi567@gmail.com>
2019-02-25 19:18:57 +0530

    Added struct for metadata and transaction

--- mongodb.go
+++ mongodb.go
@@ -15,8 +15,53 @@ type ApmDAO struct {
 var db *mgo.Database
 
 const (
-	COLLECTION = "apm-transactions"
+	TRANSACTION = "transactions"
+	METADATA    = "metadata"
 )
+
+type MongoObject struct {
+	TraceID   string `json:"trace_id"`
+	Timestamp int64  `json:"timestamp"`
+	Result    string `json:"result"`
+	Metadata  struct {
+		Service struct {
+			Name string `json:"name"`
+		} `json:"service"`
+		Version  interface{} `json:"version"`
+		Language struct {
+			Version string `json:"version"`
+			Name    string `json:"name"`
+		} `json:"language"`
+		Agent struct {
+			Version string `json:"version"`
+			Name    string `json:"name"`
+		} `json:"agent"`
+		Framework struct {
+			Version string `json:"version"`
+			Name    string `json:"name"`
+		} `json:"framework"`
+	} `json:"metadata"`
+	Request struct {
+		URL     string `json:"url"`
+		Body    string `json:"body"`
+		Headers struct {
+			ContentLength string `json:"content-length"`
+			ContentType   string `json:"content-type"`
+			Host          string `json:"host"`
+			Accept        string `json:"accept"`
+			UserAgent     string `json:"user-agent"`
+		} `json:"headers"`
+		Method string `json:"method"`
+	} `json:"request"`
+	Response struct {
+		StatusCode int `json:"status_code"`
+		Headers    struct {
+			ContentLength string `json:"Content-Length"`
+			ContentType   string `json:"Content-Type"`
+		} `json:"headers"`
+	} `json:"response"`
+	Duration float64 `json:"duration"`
+}
 
 func (m *ApmDAO) Connect() {
 	session, err := mgo.Dial(m.Server)
@@ -26,13 +71,13 @@ func (m *ApmDAO) Connect() {
 	db = session.DB(m.Database)
 }
 
-func (m *ApmDAO) FindAll() ([]Movie, error) {
-	var movies []Movie
+func (m *ApmDAO) FindAll() ([]MongoObject, error) {
+	var movies []MongoObject
 	err := db.C(COLLECTION).Find(bson.M{}).All(&movies)
 	return movies, err
 }
 
-func (m *ApmDAO) FindById(id string) (Movie, error) {
+/*func (m *ApmDAO) FindById(id string) (Movie, error) {
 	var movie Movie
 	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&movie)
 	return movie, err
@@ -51,4 +96,4 @@ func (m *ApmDAO) Delete(movie Movie) error {
 func (m *ApmDAO) Update(movie Movie) error {
 	err := db.C(COLLECTION).UpdateId(movie.ID, &movie)
 	return err
-}
+}*/
