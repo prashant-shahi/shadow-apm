fc0809d2ac8cbd714e4bb5f1b8dc37425f48cd2c
Prashant Shahi <PrashantShahi567@gmail.com>
2019-03-01 19:19:39 +0530

    Added Bulk Insert support of MongoObjects

--- helpers.go
+++ helpers.go
@@ -5,13 +5,8 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
-	"strconv"
 	"net/http"
 	"errors"
-	"reflect"
-
-	"gopkg.in/mgo.v2/bson"
-	_"github.com/mitchellh/mapstructure"
 )
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
@@ -39,14 +34,18 @@ func readAllString(r io.Reader) (res string, err error) {
 func insertMultipleTransactions(transactions []interface{}) (int, error) {
 	log.Output(0, "Function: insertMultipleTransactions")
 	if len(transactions) <= 0 {
-		log.Output(0, "No Transactions found")
-		return http.StatusCreated, errors.New("No Transactions found")
+		//log.Output(0, "No Transactions found")
+		return http.StatusOK, errors.New("No Transactions found")
 	}
 	/*if err := json.NewDecoder(r.Body).Decode(&mo); err != nil {
 		return http.StatusBadRequest, error.New("Invalid request payload")
 	}*/
 	/*vals := interface{}*/
-	for key, val := range transactions {
+	err := dao.BulkInsert(transactions)
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	/*for key, val := range transactions {
 		var mo MongoObject
 		tempJSON, err := json.Marshal(val)
 		if err != nil {
@@ -62,16 +61,10 @@ func insertMultipleTransactions(transactions []interface{}) (int, error) {
 		mo.ID = bson.NewObjectId()
 		log.Println(reflect.TypeOf(val))
 		log.Println(val)
-		/*log.Println(mo.ID)
-		log.Println("Before decode")
-		log.Println(mo)
-		mapstructure.Decode(val, &mo)
-		log.Println("After decode")
-		log.Println(mo)*/
 		if err := dao.Insert(mo); err != nil {
 			return http.StatusInternalServerError, err
 		}
 		log.Output(0, "Key "+strconv.Itoa(key)+"\tTransaction Insertion Success")
-	}
+	}*/
 	return http.StatusCreated, nil
 }
