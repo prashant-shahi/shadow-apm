6f869fa1f23a17869b6f0b664e6b629c190d48e2
Prashant Shahi <PrashantShahi567@gmail.com>
2019-03-02 12:58:37 +0530

    Cleaning up

--- helpers.go
+++ helpers.go
@@ -34,37 +34,11 @@ func readAllString(r io.Reader) (res string, err error) {
 func insertMultipleTransactions(transactions []interface{}) (int, error) {
 	log.Output(0, "Function: insertMultipleTransactions")
 	if len(transactions) <= 0 {
-		//log.Output(0, "No Transactions found")
 		return http.StatusOK, errors.New("No Transactions found")
 	}
-	/*if err := json.NewDecoder(r.Body).Decode(&mo); err != nil {
-		return http.StatusBadRequest, error.New("Invalid request payload")
-	}*/
-	/*vals := interface{}*/
 	err := dao.BulkInsert(transactions)
 	if err != nil {
 		return http.StatusInternalServerError, err
 	}
-	/*for key, val := range transactions {
-		var mo MongoObject
-		tempJSON, err := json.Marshal(val)
-		if err != nil {
-			log.Fatal(err)
-			return http.StatusInternalServerError, err
-		}
-		log.Println(string(tempJSON))
-		err = json.Unmarshal([]byte(tempJSON), &mo)
-		if err != nil {
-			log.Fatal(err)
-			return http.StatusInternalServerError, nil
-		}
-		mo.ID = bson.NewObjectId()
-		log.Println(reflect.TypeOf(val))
-		log.Println(val)
-		if err := dao.Insert(mo); err != nil {
-			return http.StatusInternalServerError, err
-		}
-		log.Output(0, "Key "+strconv.Itoa(key)+"\tTransaction Insertion Success")
-	}*/
 	return http.StatusCreated, nil
 }
